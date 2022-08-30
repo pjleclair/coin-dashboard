b86d40fb5f7780198750add94fefc8c25226a501
lompdoctor <pleclair@protonmail.com>
2022-08-29 22:15:19 -0500

    Add Info: No such file or directory Button, redesign coin layout

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -10,6 +10,9 @@ const Body = () => {
     const [coinDataFour, setCoinDataFour] = React.useState([])
     const [filterValue, setFilterValue] = React.useState(10.0)
     const [showSlider, setShowSlider] = React.useState(false)
+    const [showCoinData, setShowCoinData] = React.useState(false)
+    const [selectedCoin, setSelectedCoin] = React.useState('')
+    const [selectedCoinInfo, setSelectedCoinInfo] = React.useState({})
 
     console.log(coinList)
 
@@ -44,6 +47,24 @@ const Body = () => {
             .catch(error=>{console.log('API page 4 retrieval failed')})
     },[])
 
+    React.useEffect(() => {
+        axios
+            .get(`https://api.coingecko.com/api/v3/coins/${selectedCoin}`)
+            .then (response => {
+                console.log(response.data)
+                setSelectedCoinInfo(response.data)
+            })
+            .catch(error=>console.log(error))
+    },[selectedCoin])
+
+    const getCoinData = (event) => {
+        console.log(showCoinData)
+        if (showCoinData === false) {
+            setSelectedCoin(event.target.id)
+        }
+        setShowCoinData(prevState => !prevState)
+    }
+
     React.useEffect(()=>{
         setCoinList(coinDataOne.concat(coinDataTwo).concat(coinDataThree).concat(coinDataFour))
     },[coinDataOne,coinDataTwo,coinDataThree,coinDataFour])
@@ -52,15 +73,16 @@ const Body = () => {
         const changeVal = coin.price_change_percentage_24h
         let styles = {color:'green'}
         if (changeVal < 0) {styles={color:'red'}}
+        const imgSrc = coin.image
         return(
             <div style={{
                 display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+                gridTemplateColumns: '1fr .5fr 1fr 1fr 1fr .5fr',
                 justifyContent: 'center',
                 alignItems: 'center',
                 border: 'solid 1px black'
             }} key={i}>
-                <div style={{margin: '.5rem', fontWeight:'bold'}}>{i+1} {coin.id}:</div>
+                <div style={{margin: '.5rem', fontWeight:'bold', display: 'flex', alignItems: 'center'}}>{i+1} <img style={{height:'1rem',margin: '0 .5rem 0 .5rem'}} alt='coin logo' src={imgSrc}/> {coin.id}:</div>
                 <div>{coin.symbol}</div>
                 <div>{coin.current_price}</div>
                 <div>{coin.market_cap}</div>
@@ -77,19 +99,26 @@ const Body = () => {
         const changeVal = coin.price_change_percentage_24h
         let styles = {color:'green'}
         if (changeVal < 0) {styles={color:'red'}}
+        const imgSrc = coin.image
         return(
             <div style={{
                 display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+                gridTemplateColumns: '1fr 1fr 1fr 1fr .5fr',
                 justifyContent: 'center',
                 alignItems: 'center',
                 border: 'solid 1px black'
             }} key={i}>
-                <div style={{margin: '.5rem', fontWeight:'bold'}}>{i+1} {coin.id}:</div>
-                <div>{coin.symbol}</div>
+                <div style={{display:'flex', alignItems:'center'}}>
+                    <div style={{marginLeft:'.5rem',fontWeight:'bold', display: 'flex', alignItems: 'center'}}>{i+1} <img style={{height:'1rem',margin: '0 .5rem 0 .5rem'}} alt='coin logo' src={imgSrc}/></div>
+                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
+                        <div style={{fontWeight:'bold', display: 'flex', alignItems: 'center'}}>{coin.id}:</div>
+                        <div>{coin.symbol}</div>
+                    </div>
+                </div>
                 <div>{coin.market_cap}</div>
                 <div>{coin.current_price}</div>
                 <div style={styles}>{coin.price_change_percentage_24h}%</div>
+                <button id={coin.id} onClick={getCoinData}>More info</button>
             </div>
         )
     })
@@ -103,61 +132,97 @@ const Body = () => {
         setFilterValue(event.target.value)
     }
 
+    const MarketStats = () => {
+        return (
+            <>
+                <h1 style={{
+                    display: 'flex',
+                    justifyContent:'center'
+                }}>Market Stats</h1>
+            </>
+        )
+    }
+
+    const CoinData = ({coin}) => {
+        // const site = coin.links.homepage[0]
+        console.log(coin)
+        // const desc = props.coin.description.en
+        return (
+            <>
+                <div style={{display: 'flex'}}>
+                    <button onClick={getCoinData}>Return</button>
+                    <a style={{marginLeft: 'auto'}} href={coin.links.homepage[0]}>Website</a>
+                </div>
+                <h1>{coin.name}</h1>
+                <p>{coin.description.en}</p>
+            </>
+        )
+    }
+
     return (
         <div style={{
             padding: '1rem'
         }}>
-            <h1>Tokens with 24h price change &gt;{filterValue}%:</h1>
-            <div style={{
-                display: 'flex',
-                alignItems: 'center'
-            }}>
-                <button onClick={handleClick} style={{
-                    marginRight:'1rem', 
-                    display: 'inline-block',
-                    outline: '0',
-                    border: '0',
-                    cursor: 'pointer',
-                    backgroundColor: 'lightblue',
-                    borderRadius: '50px',
-                    padding: '8px 16px',
-                    fontSize: '16px',
-                    fontWeight: '700',
-                    color: 'white',
-                    lineHeight: '26px'
-                }}>Adjust Value</button>
-                {showSlider === true ? 
-                    <div>
-                        <input type={'range'} min='1' max='20' value={filterValue} name='filterValue' onChange={handleChange}
-                        ></input>
-                    </div> : 
-                    <div></div>
-                }
-            </div>
-            <div style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
-                fontWeight: 'bolder'
-            }}>
-                <h2>Token</h2>
-                <h2>Symbol</h2>
-                <h2>Market Cap</h2>
-                <h2>Price</h2>
-                <h2>24h Price Change</h2>
-            </div>
-            {topChangesArray}
-            <h1>Top tokens by MarketCap:</h1>
-            <div style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'
-            }}>
-                <h2>Token</h2>
-                <h2>Symbol</h2>
-                <h2>Current Price</h2>
-                <h2>Market Cap</h2>
-                <h2>24h Price Change</h2>
-            </div>
-            {displayArray}
+            {showCoinData === true ? 
+            <CoinData 
+                coin={selectedCoinInfo}
+            /> :
+            <>
+                <MarketStats />
+                <h1>Tokens with 24h price change &gt;{filterValue}%:</h1>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    <button onClick={handleClick} style={{
+                        marginRight:'1rem', 
+                        display: 'inline-block',
+                        outline: '0',
+                        border: '0',
+                        cursor: 'pointer',
+                        backgroundColor: 'lightblue',
+                        borderRadius: '50px',
+                        padding: '8px 16px',
+                        fontSize: '16px',
+                        fontWeight: '700',
+                        color: 'white',
+                        lineHeight: '26px'
+                    }}>Adjust Filter</button>
+                    {showSlider === true ? 
+                        <div>
+                            <input type={'range'} min='1' max='20' value={filterValue} name='filterValue' onChange={handleChange}
+                            ></input>
+                        </div> : 
+                        <div></div>
+                    }
+                </div>
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: '1fr 1fr 1fr 1fr .5fr',
+                    fontWeight: 'bolder'
+                }}>
+                    <h2>Token</h2>
+                    <h2>Market Cap</h2>
+                    <h2>Price</h2>
+                    <h2>24h Price Change</h2>
+                    <h2>More Info</h2>
+                </div>
+                {topChangesArray}
+                <h1>Top tokens by MarketCap:</h1>
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: '1fr .5fr 1fr 1fr 1fr .5fr'
+                }}>
+                    <h2>Token</h2>
+                    <h2>Symbol</h2>
+                    <h2>Current Price</h2>
+                    <h2>Market Cap</h2>
+                    <h2>24h Price Change</h2>
+                    <h2>More Info</h2>
+                </div>
+                {displayArray}
+            </>
+            }
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,18 +4,17 @@ const Header = () => {
     const styles = {
         color:'lightblue',
         border:'solid black 1px',
-        borderRadius:'20px',
-        padding: '1rem',
+        padding: '.5rem',
         display: 'flex',
         alignItems: 'center',
         boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
     }
 
     return (
-        <div style={styles}>
+        <nav style={styles}>
             <h1>Coin Dashboard</h1>
             <div style={{marginLeft:'auto',color:'black'}}>supported by CoinGecko API</div>
-        </div>
+        </nav>
     )
 }
 
