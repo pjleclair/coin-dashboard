930ed2be0ca1eed7af8ff5074b1e04d6bf400cba
lompdoctor <pleclair@protonmail.com>
2022-09-24 11:51:59 -0500

    Implement sidebar-button & CSS animation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,10 +11,23 @@ const Header = () => {
         borderRadius: '0 0 10px 10px'
     }
 
+    const handleClick = (e) => {
+        const target = document.getElementById('sidebar')
+        if (target.className === 'sidebar-button') {
+            target.className = 'sidebar-button-clicked'
+        } else {
+            target.className = 'sidebar-button'
+        }
+    }
+
     return (
         <nav style={styles}>
-            <h1 style={{color:'lightblue'}}>Coin Dashboard</h1>
-            <div style={{marginLeft:'auto',color:'white'}}>supported by CoinGecko API</div>
+            <div id='sidebar' className='sidebar-button' onClick={handleClick} style={{display: 'flex', flexDirection: 'column', gap: '.5rem'}}>
+                <div style={{backgroundColor:'white', width: '2rem', height: '.5rem'}}></div>
+                <div style={{backgroundColor:'white', width: '2rem', height: '.5rem'}}></div>
+                <div style={{backgroundColor:'white', width: '2rem', height: '.5rem'}}></div>
+            </div>
+            <h1 style={{marginLeft:'auto', color:'lightblue'}}>Coin Dashboard</h1>
         </nav>
     )
 }
